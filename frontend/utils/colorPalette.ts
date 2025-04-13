38740f94aff11fd11c4db16b4adb34da8879d075
jauresmanouan <jauresmanouan@gmail.com>
2025-04-13 16:20:25 +0000

    ⚡️ Possibilité de choisir la couleur du map dans le composant mais pas encore dynamique

---
+++ frontend/utils/colorPalette.ts
@@ -0,0 +1,19 @@
+// 🎨 Fonction palette
+export const colorPalette: Record<string, string> = {
+  viridis: "from-[#440154] via-[#21908C] to-[#FDE725]",
+  plasma: "from-[#0D0887] via-[#D8576B] to-[#F0F921]",
+  inferno: "from-[#000004] via-[#BC3754] to-[#FCFFA4]",
+  magma: "from-[#000004] via-[#B53679] to-[#FBFDBC]",
+  cividis: "from-[#00224E] via-[#7D7D7D] to-[#FFE945]",
+  Blues: "from-blue-100 via-blue-400 to-blue-800",
+  Reds: "from-red-100 via-red-400 to-red-800",
+  Greens: "from-green-100 via-green-400 to-green-800",
+  turbo: "from-yellow-300 via-pink-500 to-purple-900",
+  jet: "from-blue-700 via-yellow-500 to-red-700",
+  rainbow:
+    "from-red-500 via-yellow-400 via-green-400 via-blue-400 to-purple-500",
+  gray: "from-gray-100 via-gray-400 to-gray-700",
+  hot: "from-red-200 via-orange-500 to-black",
+  cool: "from-cyan-300 via-blue-400 to-purple-600",
+  terrain: "from-green-600 via-yellow-400 to-brown-600",
+};
