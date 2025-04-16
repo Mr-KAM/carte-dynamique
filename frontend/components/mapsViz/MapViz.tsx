74d92e8fbf7d66fb9044590de2522c1e84638d6d
jauresmanouan <jauresmanouan@gmail.com>
2025-04-16 03:26:36 +0000

    ⚡️ Modification dynamique des paramêtres de la map

--- frontend/components/mapsViz/MapViz.tsx
+++ frontend/components/mapsViz/MapViz.tsx
@@ -7,27 +7,34 @@ export default function MapViz() {
   const { title, selectedPalette, legend } = useCustomMapStore();
 
   useEffect(() => {
-    const MapImageStored = sessionStorage.getItem("mapImage");
+    const currentParams = JSON.stringify({ title, selectedPalette, legend });
 
-    if (MapImageStored) {
-      setImageSrc(MapImageStored);
-    } else {
-      fetch(
-        `http://localhost:8000/generate-map?title=${encodeURIComponent(
-          title
-        )}&cmap=${selectedPalette}&label_title=${encodeURIComponent(legend)}`
-      )
-        .then((res) => res.blob())
-        .then((imageBlob: Blob) => {
-          const imageObjectUrl = URL.createObjectURL(imageBlob);
-          setImageSrc(imageObjectUrl);
-          sessionStorage.setItem("mapImage", imageObjectUrl);
-        })
-        .catch((error) =>
-          console.error("Erreur lors de la récupération d'image", error)
-        );
-    }
-  }, []);
+    const timeout = setTimeout(() => {
+      const MapImageStored = sessionStorage.getItem("mapImage");
+      const MapParamsStored = sessionStorage.getItem("mapImageParams");
+
+      if (MapImageStored && MapParamsStored === currentParams) {
+        setImageSrc(MapImageStored);
+      } else {
+        fetch(
+          `http://localhost:8000/generate-map?title=${encodeURIComponent(
+            title
+          )}&cmap=${selectedPalette}&label_title=${encodeURIComponent(legend)}`
+        )
+          .then((res) => res.blob())
+          .then((imageBlob: Blob) => {
+            const imageObjectUrl = URL.createObjectURL(imageBlob);
+            setImageSrc(imageObjectUrl);
+            sessionStorage.setItem("mapImage", imageObjectUrl);
+            sessionStorage.setItem("mapImageParams", currentParams);
+          })
+          .catch((error) =>
+            console.error("Erreur lors de la récupération d'image", error)
+          );
+      }
+    }, 600);
+    return () => clearTimeout(timeout);
+  }, [title, selectedPalette, legend]);
 
   return (
     <img
