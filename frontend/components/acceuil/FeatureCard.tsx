0cdfe54523f9b0e161fdc8a2b2cb16f088d4e870
jauresmanouan <jauresmanouan@gmail.com>
2025-04-22 15:41:20 +0000

    ♻️ Amélioration du code de la partie Feature

---
+++ frontend/components/acceuil/FeatureCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+export default function FeatureCard({
+  icon: Icon,
+  title,
+  description,
+}: {
+  icon: React.ElementType;
+  title: string;
+  description: string;
+}) {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center gap-4">
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary/10">
+          <Icon className="h-6 w-6 text-primary" />
+        </div>
+        <div className="space-y-1">
+          <CardTitle>{title}</CardTitle>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
--- frontend/components/acceuil/FeaturesSection.tsx
+++ frontend/components/acceuil/FeaturesSection.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
-import { BarChart3, BotMessageSquare, Globe, Link } from "lucide-react";
-import { Button } from "../ui/button";
+import { BarChart3, BotMessageSquare, Globe } from "lucide-react";
+import FeatureCard from "./FeatureCard";
 
 export default function FeaturesSection() {
+  const features = [
+    {
+      icon: BarChart3,
+      title: "Tableaux de Bord",
+      description:
+        "Visualisez les indicateurs clés de performance à travers des tableaux de bord interactifs et personnalisables.",
+    },
+    {
+      icon: Globe,
+      title: "Cartes Interactives",
+      description:
+        "Explorez les données géographiques à travers des cartes interactives pour une meilleure compréhension territoriale.",
+    },
+    {
+      icon: BotMessageSquare,
+      title: "Assistant IA",
+      description:
+        "Posez des questions en langage naturel et obtenez des analyses et des visualisations générées par notre assistant IA.",
+    },
+  ];
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 bg-green-400">
       <div className="px-4 md:px-6">
@@ -18,7 +31,7 @@ export default function FeaturesSection() {
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
               Notre Plateforme
             </h2>
-            <p className="max-w-[900px] text-slate-200 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p className="max-w-[900px] text-slate-50 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Découvrez les fonctionnalités qui vous permettent d&apos;analyser
               et de visualiser les données des ministères de Côte d&apos;Ivoire.
             </p>
@@ -26,61 +39,14 @@ export default function FeaturesSection() {
         </div>
 
         <div className="mx-auto grid max-w-5xl grid-cols-1 gap-6 md:grid-cols-3 lg:gap-12 mt-8">
-          <Card>
-            <CardHeader className="flex flex-row items-center gap-4">
-              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary/10">
-                <BarChart3 className="h-6 w-6 text-primary" />
-              </div>
-              <div className="space-y-1">
-                <CardTitle>Tableaux de Bord</CardTitle>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm text-muted-foreground">
-                Visualisez les indicateurs clés de performance à travers des
-                tableaux de bord interactifs et personnalisables.
-              </p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader className="flex flex-row items-center gap-4">
-              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary/10">
-                <Globe className="h-6 w-6 text-primary" />
-              </div>
-              <div className="space-y-1">
-                <CardTitle>Cartes Interactives</CardTitle>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm text-muted-foreground">
-                Explorez les données géographiques à travers des cartes
-                interactives pour une meilleure compréhension territoriale.
-              </p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader className="flex flex-row items-center gap-4">
-              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary/10">
-                <BotMessageSquare className="h-6 w-6 text-primary" />
-              </div>
-              <div className="space-y-1">
-                <CardTitle>Assistant IA</CardTitle>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm text-muted-foreground">
-                Posez des questions en langage naturel et obtenez des analyses
-                et des visualisations générées par notre assistant IA.
-              </p>
-              <CardFooter>
-                <Button variant="ghost" size="sm" asChild>
-                  <Link href="">Explorer</Link>
-                </Button>
-              </CardFooter>
-            </CardContent>
-          </Card>
+          {features.map((feature, index) => (
+            <FeatureCard
+              key={index}
+              icon={feature.icon}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
         </div>
       </div>
     </section>
