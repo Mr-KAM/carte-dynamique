753be808a36b5c9396769a7ea580285b411b23df
jauresmanouan <jauresmanouan@gmail.com>
2025-04-26 16:17:29 +0000

    ♻️ ✨ Creation et integration de l'api chat ainsi que refacto du code

--- backend/rag-backend/app.py
+++ backend/rag-backend/app.py
@@ -1,46 +1,22 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_community.chat_models import ChatOllama
-from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from rag_process import rag_process
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
-pdf_path = "/Users/macbookair/Desktop/DPSI/Annuaires Statistiques METFPA/Annuaire 2024/Annuaire Stat 2023-2024_v14_21042025.pdf"
-
-#Charger le document pdf
-loader = PyPDFLoader(pdf_path)
-
-#Découper le pdf
-pages = loader.load_and_split()
-
-pages = loader.load()
-
-embeddings = OllamaEmbeddings(model="bge-m3")
-
-vdb = Chroma.from_documents(pages, embeddings)
-
-template = """
-Réponds dans la langue dans laquelle la question est posée, en t'appuyant uniquement
-sur le contexte fourni sans ajouter d'éléments. Si la réponse n'est pas disponible
-Indique : Cette information ne se trouve pas encore dans les documents à ma connaissance.
-Reponds surtout de manière courtoise et agréble en ajoutant des émoticones.
-
-Contexte : {context}
-Question : {question} [/INST] 
-""" 
-
-QA_PROMPT = PromptTemplate.from_template(template)
-
-llm = ChatOllama(model="mistral:7b-instruct", temperature=0)
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm = llm,
-    chain_type = "stuff",
-    retriever = vdb.as_retriever(),
-    chain_type_kwargs = {"prompt": QA_PROMPT}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-question = "Quel est le nombre d'apprenant fille en couture ?"
-response = qa_chain.run(question)
-response
+pdf_path = "/Users/macbookair/Desktop/DPSI/Annuaires Statistiques METFPA/Annuaire 2024/Annuaire Stat 2023-2024_v14_21042025.pdf"
+qa_chain = rag_process(pdf_path)
+ 
+@app.post("/chat")
+async def ask_qestion(question: str= Body(...)):
+    response = qa_chain.run(question)
+    return {"Response": response }
---
+++ backend/rag-backend/rag_process.py
@@ -0,0 +1,41 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.chat_models import ChatOllama
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+
+def rag_process(pdf_path: str):
+    
+    #Charger le document pdf
+    loader = PyPDFLoader(pdf_path)
+
+    #Découper le pdf
+    pages = loader.load_and_split()
+
+    embeddings = OllamaEmbeddings(model="bge-m3")
+
+    vdb = Chroma.from_documents(pages, embeddings)
+
+    template = """
+    Réponds dans la langue dans laquelle la question est posée, en t'appuyant uniquement
+    sur le contexte fourni sans ajouter d'éléments. Si la réponse n'est pas disponible
+    Indique : Cette information ne se trouve pas encore dans les documents à ma connaissance.
+    Reponds surtout de manière courtoise et agréble en ajoutant des émoticones.
+
+    Contexte : {context}
+    Question : {question} [/INST] 
+    """ 
+
+    QA_PROMPT = PromptTemplate.from_template(template)
+
+    llm = ChatOllama(model="mistral:7b-instruct", temperature=0)
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever = vdb.as_retriever(),
+        chain_type_kwargs = {"prompt": QA_PROMPT}
+    )
+    
+    return qa_chain
